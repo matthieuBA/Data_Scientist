2d1103e8d818cd65cd3be3b8b09817e3ed884a4d
matthieuBA <matthieubacheley@gmail.com>
2020-04-10 08:12:45 +0200

    added method into journalists

--- lib/00_journalists_SAV.rb
+++ lib/00_journalists_SAV.rb
@@ -1,147 +1,92 @@
 tab = ["@jcunniet", "@PaulLampon", "@Aziliz31", "@ssoumier", "@marionsouzeau", "@gaellombart", "@bendarag", "@AurelieLebelle", "@julienduffe", "@thomaspoupeau", "@LilyRossignol", "@ClairGuedon", "@stephanieauguy", "@claw_prolongeau", "@_JulieMenard", "@LColcomb", "@Zlauwereys", "@MeLonguet", "@DorotheeLN", "@NolwennCosson", "@ADaboval", "@Remibaldy", "@bderveaux", "@amandechap", "@ELODIESOULIE", "@nbongarcon", "@HeloAb", "@Panamorama", "@gregplou", "@BenoitBerthe", "@LauraBruneau89", "@Anthony_Lieures", "@Sharonwaj", "@mcsonkin", "@pverduzier", "@emiliel3", "@Julien_MARION", "@SophiFay", "@bdelombre", "@annalecerf", "@AdriaBudry", "@DejNikolic", "@iJaffre", "@CyrusleVirus", "@GPardigon", "@e_vallerey", "@IsabelleSouquet", "@AudeDavidRossi", "@Yoann_Pa", "@CeliaPenavaire", "@perraultvincent", "@cboulate", "@JustineWeyl", "@Paulinejacot", "@juliechab", "@aslechevallier", "@phnou", "@Seb_Pommier", "@Alex_Bensaid", "@GuillaumeGaven", "@annelaurechouin", "@Oliviader", "@guerricp", "@JMMarchaut", "@cyceron", "@gregory_raymond", "@vhunsinger", "@l_peillon", "@fannyguinochet", "@EAssayag", "@KibweAdom", "@YvonHerry", "@JohanGarciajg", "@saidlabidi", "@lauranneprov", "@LeaDavy", "@francois_remy", "@CGuicheteau", "@FloMaillet", "@m_perroud", "@oBrunet_TSMF", "@MoonVdc", "@jc2taille", "@NellyMoussu", "@VirginK", "@b_misa", "@FabriceCouste", "@barbara_prose", "@lelia2m", "@brunoaskenazi", "@laurenechamp", "@ysisbox", "@juliengagliardi", "@PierreLel", "@kdeniau", "@_TerraInc", "@DominicArpin", "@antoinfonteneau", "@nanotousch", "@jb_roch", "@YaniKhezzar", "@Anne_Bechet", "@NCapart", "@SamyBenNaceur", "@Joumany", "@Julietteraynal", "@TGiraudet", "@SaraTanit", "@HappeFrederic", "@antoinellorca", "@michelpicot", "@Sev_Ryne", "@bobdobolino", "@murdever", "@YGrandmontagne", "@Mnyo", "@EdKOSCIANSKI", "@tnoisette", "@jankari", "@delbello_rom", "@rflechaux", "@NadiaSorelli", "@IT_Digital", "@abarbaux", "@PhilippeLeroy", "@schaptal", "@marionspee", "@lisavignoli", "@ChloeAeberhardt", "@MartineJacot", "@JuliaPascualita", "@curieusedetout", "@sgraveleau", "@bif_o", "@ElisaPineau", "@zinebdryef", "@apiquard", "@pierrehaski", "@StephanieDelmas", "@Blandine_Garot", "@vergara_i", "@evan_lebastard", "@SophieVclt", "@OlivierLevrault", "@alicedorgeval", "@LouiseMalnoy", "@alix_fx", "@pierre_baudis", "@LucMagoutier", "@AgatheMuller", "@SGianninelli", "@PaulineBoyer33", "@NaomiHalll", "@romaindlx", "@marionbr", "@Burtschy", "@JacobEtienne", "@as_lizzani", "@marie_simon", "@LaureDaussy", "@FabriceAmedeo", "@LoubnaChlaikhy", "@PlummerWilliam", "@OlivierMarin1", "@alaurefremont", "@mwesfreid", "@ChBaDe", "@pmathon", "@theobaldmarie", "@Lnpagesy", "@marclandre", "@paoliniesther", "@Feertchak", "@JBLitzler", "@GuillaumeErrard", "@quentinperinel", "@TristanQM", "@mlbo", "@constancejamet", "@LoraTerrazas", "@emiliegeffray", "@Mathilde_Sd", "@CaroPiquet", "@DCanivez", "@TIM_7375", "@blandinelc", "@ivanrioufol", "@arthurberdah", "@SarahLecoeuvre", "@guillaume_gui", "@DamienMercereau", "@W_Chloe", "@Assma_MD", "@EugenieBastie", "@HiTech_lexpress", "@bcondominas", "@Laurie_Z_", "@jeanfrancgerard", "@MathieuPagura", "@BGUYF", "@AlainPiffaretti", "@AudreyKucinskas", "@julienhory", "@Pierrefalga", "@TiphThuillier", "@cdaniez", "@LigerBaptiste", "@D_Peras", "@julie_dlb", "@Fatiha_Temmouri", "@julian2lamancha", "@GaetaneDeljurie", "@JulianMattei", "@M_Vicuna", "@DeBruynOlivier", "@Nehed_Jendoubi", "@antoine_grenapi", "@ColonnaGen", "@VictoriaGairin", "@Clement_Lacombe", "@TVigoureux", "@MargauxObriot", "@solinedelos", "@RocheSabine", "@dangerkens", "@EdouardDutour", "@MDondeyne", "@DupuisNathalie1", "@bouscarel", "@Mathieu2jean", "@Sophie_T_J", "@laurentcalixte", "@patrockwilliams", "@PascaleKremer", "@AlexJaquin", "@LauraIsaaz", "@cath_robin", "@Del_Gautherin", "@Isaduriez", "@lucietuile", "@AugeyBastien", "@mcastagnet", "@AminaKalache", "@mvaudano", "@CParrot", "@ombelinetips", "@_JoinLion", "@BarbolosiRose", "@ToiBruno1", "@FloraClodic", "@xjbdx", "@AlexiaEy", "@Emjy_STARK", "@elcoco01", "@rabilebon", "@pflovens_", "@FabriceFrossard", "@MorganeSERRES", "@MarjolaineKoch", "@edgarsnow", "@SRNLF", "@CChassigneux", "@KerinecMoran", "@NassiraELM", "@NewsRicard", "@Sandreene", "@Emilezrt", "@Pierre_Do", "@Micode", "@CColumelli", "@DavidAbiker", "@ClementBergantz", "@benjaminrabier", "@celinekallmann", "@edwyplenel", "@C_Barbier", "@JJBourdin_RMC", "@LaurenceFerrari", "@aslapix", "@IsaMillet", "@MaximeSwitek", "@tomjoubert", "@jszanchi", "@roqueeva", "@XavierBiseul", "@florencesantrot", "@AntoineCrochet", "@freeman59", "@MaudeML", "@philippe_gulpi", "@mathieum76", "@kiouari", "@imanemoustakir", "@BenedicteMallet", "@Emilie_Brouze", "@antoinebarret", "@_nicolasbocquet", "@remibuhagiar", "@CourretB", "@AymericRobert", "@miraelmartins", "@pmaniere", "@jesuisraphk", "@David_Ingram", "@pcelerier", "@technomedia", "@Geraldinedauver", "@ThierryLabro", "@Newsdusud", "@nrauline", "@gbregeras", "@SCouasnonBFM", "@actualites_nrv", "@dimitrimoulins", "@oli_aura", "@FabieChiche", "@Vincent_Raimblt", "@ChristophGreuet", "@PAlbuchay", "@MarrauddesGrot", "@vtalmon", "@cedric", "@olivierfrigara", "@Julien_Jay", "@LydiaBerroyer", "@Shuua", "@datisdaz", "@Steuph", "@ameliecharnay", "@Bruno_LesNums", "@LelloucheNico", "@CciliaDiQuinzio", "@Elodie_C", "@SylvRolland", "@Kocobe", "@FL_Debes", "@jdupontcalbo", "@GarciaVictor_", "@NicoRichaud", "@RHoueix", "@simottel", "@DamienLicata", "@annabelle_L", "@Lea_Lejeune", "@axel_deb", "@marin_eben", "@ptiberry", "@MatthieuDelach", "@sandrinecassini", "@benjaminferran", "@ppgarcia75", "@NotPatrick", "@ivalerio", "@FabienneSchmitt", "@alexgoude", "@JeromeColombain", "@manhack", "@Capucine_Cousin", "@Fsorel", "@oliviertesquet", "@marjoriepaillon", "@ginades", "@PierreTran", "@DelphineCuny", "@reesmarc", "@lauratenoudji", "@ldupin", "@carolinedescham", "@Lucile_Quillet", "@cgabizon", "@Allocab", "@epenser", "@JAGventeprivee", "@frwrds", "@Laure__Bourdon", "@Xavier75", "@maximeverner", "@s_jourdain", "@LoriHelloc"]
 
 #question 1: nombre de journalistes dans l'array
-def number_handle_counter(tab)
-  number_handle = 0
-  number_handle = tab.length
-  puts number_handle
-  return number_handle
-end
 
-# number_handle = number_handle_counter(tab)
+number_handle = tab.length
+puts number_handle
 
 #nombre de handle avec un nombre
-def number_handle_with_number(tab, number_handle)
-  nb = 0
-  tab.each do |var|
-    # puts var
-    if var =~ /\d/ # Calling String's =~ method.
-      # puts "The String #{var} has a number in it."
-      nb += 1
-    else
-      # puts "The String #{var} does not have a number in it."
-    end
+nb = 0
+tab.each do |var|
+  puts var
+  if var =~ /\d/ # Calling String's =~ method.
+    puts "The String #{var} has a number in it."
+    nb += 1
+  else
+    # puts "The String #{var} does not have a number in it."
   end
-  puts "les #{number_handle} Handles contiennent #{nb} nombres"
 end
-
-# number_handle_with_number(tab, number_handle)
+puts "les #{number_handle} Handles contiennent #{nb} nombre"
 
 #handle contiennent les 4 lettres du prénom "Aude" à la suite
-def number_handle_with_aude(tab, number_handle)
-  nb = 0
-  tab.each do |var|
-    var = var.downcase
-    if var.include? "aude"
-      # puts "String includes 'aude'"
-      nb += 1
-    end
+nb = 0
+tab.each do |var|
+  var = var.downcase
+  if var.include? "aude"
+    # puts "String includes 'aude'"
+    nb += 1
   end
-  puts "les #{number_handle} Handles contiennent #{nb} fois le nom aude"
 end
-
-# number_handle_with_aude(tab, number_handle)
+puts "les #{number_handle} Handles contiennent #{nb} fois le nom aude"
 
 #nombre de handles commencant par une majuscule après le "@"
-def number_handle_with_upper_first(tab, number_handle)
-  nb = 0
-  tab.each do |var|
-    # print var.chars
-    char = var[1]
-    if char === char.capitalize
-      # puts 'Character is uppercase.'
-      nb += 1
-    else
-      # puts 'Character is lowercase.'
-    end
-    # puts
+nb = 0
+tab.each do |var|
+  print var.chars
+  char = var[1]
+  if char === char.capitalize
+    # puts 'Character is uppercase.'
+    nb += 1
+  else
+    # puts 'Character is lowercase.'
   end
-  puts "les #{number_handle} Handles contiennent #{nb} fois une majuscule en premiere lettre"
+  puts
 end
-
-# number_handle_with_upper_first(tab, number_handle)
+puts "les #{number_handle} Handles contiennent #{nb} fois une majuscule en premiere lettre"
 
 #nombre de handles contenant une majuscule
-def number_handle_with_upper(tab, number_handle)
-  nb = 0
-  tab.each do |var|
-    n = var.count("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    if n >= 1
-      nb += 1
-    end
+nb = 0
+tab.each do |var|
+  n = var.count("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+  if n >= 1
+    nb += 1
   end
-  puts "les #{number_handle} Handles contiennent #{nb} fois une majuscule au moins"
 end
-
-# number_handle_with_upper(tab, number_handle)
+puts "les #{number_handle} Handles contiennent #{nb} fois une majuscule au moins"
 
 #Combien y a-t-il de underscore _ dans tous les pseudos confondus
-def number_underscore(tab, number_handle)
-  nb = 0
-  tab.each do |var|
-    n = var.count("_")
-    # puts "#{var}  #{n}"
-    nb += n
-  end
-  puts "les #{number_handle} Handles contiennent #{nb} fois un underscore"
-end
 
-# number_underscore(tab, number_handle)
+nb = 0
+tab.each do |var|
+  n = var.count("_")
+  # puts "#{var}  #{n}"
+  nb += n
+end
+puts "les #{number_handle} Handles contiennent #{nb} fois un underscore"
 
 #Combien y a-t-il de pseudo contenant au moins un underscore _ dans tous les pseudos confondus
-def number_handle_with_underscore(tab, number_handle)
-  nb = 0
-  tab.each do |var|
-    n = var.count("_")
-    if n >= 1
-      nb += 1
-    end
+nb = 0
+tab.each do |var|
+  n = var.count("_")
+  if n >= 1
+    nb += 1
   end
-  puts "les #{number_handle} Handles contiennent #{nb} qui présentent au moins un underscore"
 end
-
-# number_handle_with_underscore(tab, number_handle)
+puts "les #{number_handle} Handles contiennent #{nb} qui présentent au moins un underscore"
 
 # tri alphabet
 # tab=tab.sort { |a, b| a.upcase <=> b.upcase }
-def handle_alpha(tab, number_handle)
-  tab_s_a = tab.sort do |a, b|
-    a.upcase <=> b.upcase
-  end
-  puts "Handle par ordre alpha ="
-  print tab_s_a
-  puts
+tab_s_a = tab.sort do |a, b|
+  a.upcase <=> b.upcase
 end
-
-# handle_alpha(tab, number_handle)
+puts "Handle par ordre alpha ="
+print tab_s_a
+puts
 
 #tri by length a
-def handle_by_length(tab, number_handle)
-  tab_s_l = tab.sort_by(&:length)
-  puts "50 Plus court="
-  puts tab_s_l[0, 50]
-end
 
-# handle_by_length(tab, number_handle)
-
-def handle_epenser(tab, number_handle)
-  puts
-  puts "le Handle @epenser se trouve a l'index #{tab.index("@epenser")} sur #{number_handle}"
-end
-
-# handle_epenser(tab, number_handle)
-
-def perform(tab, number_handle)
-  number_handle_counter(tab)
-  number_handle_with_number(tab, number_handle)
-  number_handle_with_aude(tab, number_handle)
-  number_handle_with_upper_first(tab, number_handle)
-  number_handle_with_upper(tab, number_handle)
-  number_underscore(tab, number_handle)
-  number_handle_with_underscore(tab, number_handle)
-  handle_alpha(tab, number_handle)
-  handle_by_length(tab, number_handle)
-  handle_epenser(tab, number_handle)
-end
-
-perform(tab, tab.length)
+tab_s_l = tab.sort_by(&:length)
+puts "50 Plus court="
+puts tab_s_l[0, 50]
+puts
+puts "le Handle @epenser se trouve a l'index #{tab.index("@epenser")} sur #{number_handle}"
